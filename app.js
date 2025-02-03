980c5bba4d5d656b4b142d86250e94d7a051cdb8
lucasthenrique <teodorolusca.dev@gmail.com>
2025-02-03 11:27:58 -0300

    botão adicionar e sortear amigo

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeAmigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+
+    if (listaDeAmigos.includes(nome)) {
+        alert('Esse nome já está na lista de amigos!')
+    } else {
+    listaDeAmigos.push(nome);
+    limparCampo();
+    }
+    
+}
+
+function sortearAmigo() {
+    
+if (listaDeAmigos.length == 0) {
+    alert('A lista está vazia!');
+    return;
+}
+    let nomeSorteado = Math.floor(Math.random() * listaDeAmigos.length);
+    alert(listaDeAmigos[nomeSorteado]);
+;}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
